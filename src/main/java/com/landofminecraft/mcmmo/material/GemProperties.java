5dbe77056db2a2b1ead8077a7779d0ce8e191341
Cadiboo <37298044+Cadiboo@users.noreply.github.com>
2018-10-15 18:15:01 +1100

    Update src/main/java/com/landofminecraft/mcmmo/material/GemProperties.java

--- src/main/java/com/landofminecraft/mcmmo/material/GemProperties.java
+++ src/main/java/com/landofminecraft/mcmmo/material/GemProperties.java
@@ -14,13 +14,15 @@ import net.minecraft.util.BlockRenderLayer;
 public class GemProperties extends ModMaterialProperties {
 
 	public static final BlockRenderLayer[] BLOCK_RENDER_LAYERS = new BlockRenderLayer[]{BlockRenderLayer.SOLID, BlockRenderLayer.TRANSLUCENT};
+	public static final String RESOURCE_SUFFIX = "";
+	public static final String RESOURCE_PIECE_SUFFIX = "shard";
 
 	public GemProperties(final boolean hasOre, final float MOHS_Hardness, @Nullable final Supplier<Item> getOreDrop, @Nonnull @MethodsReturnNonnullByDefault final BiFunction<Integer, Random, Integer> getQuantityDroppedWithBonus) {
-		super(hasOre, true, true, "", true, "shard", true, true, true, true, true, true, true, true, true, true, MOHS_Hardness, getOreDrop, BLOCK_RENDER_LAYERS, getQuantityDroppedWithBonus);
+		this(hasOre, MOHS_Hardness, getOreDrop, BLOCK_RENDER_LAYERS, getQuantityDroppedWithBonus);
 	}
 
 	public GemProperties(final boolean hasOre, final float MOHS_Hardness, @Nullable final Supplier<Item> getOreDrop, @Nullable final BlockRenderLayer[] blockRenderLayers, @Nonnull @MethodsReturnNonnullByDefault final BiFunction<Integer, Random, Integer> getQuantityDroppedWithBonus) {
-		super(hasOre, true, true, "", true, "shard", true, true, true, true, true, true, true, true, true, true, MOHS_Hardness, getOreDrop, blockRenderLayers, getQuantityDroppedWithBonus);
+		super(hasOre, true, true, RESOURCE_SUFFIX, true, RESOURCE_PIECE_SUFFIX, true, true, true, true, true, true, true, true, true, true, MOHS_Hardness, getOreDrop, blockRenderLayers, getQuantityDroppedWithBonus);
 	}
 
 }
